434290b1568e5cda212d21444704efd1bda8d51e
Lakshay Aggarwal <138527576+lakshay-1809@users.noreply.github.com>
2024-04-10 16:38:23 +0530

    files uploaded

---
+++ politics.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+    numb: 1,
+    question: "The name of the Laccadive, Minicoy and Amindivi islands was changed to Lakshadweep by an Act of Parliament in?",
+    answer: "1973",
+    options: [
+      "1973",
+      "1972",
+      "1971",
+      "1970"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The members of the Rajya Sabha are elected by",
+    answer: "elected members of the legislative assembly",
+    options: [
+      "the people",
+      "Lok Sabha",
+      "elected members of the legislative assembly",
+      "elected members of the legislative council"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The members of the panchayat are",
+    answer: "the electorates of the respective territorial constituencies",
+    options: [
+      "nominated by the district officer",
+      "the electorates of the respective territorial constituencies",
+      "nominated by local self-government minister of the state",
+      "nominated by the block development organization"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The power to decide an election petition is vested in the",
+    answer: "High courts",
+    options: [
+      "Parliament",
+      "Supreme Court",
+      "High courts",
+      "Election Commission"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The present Lok Sabha is the",
+    answer: "17th Lok Sabha",
+    options: [
+      "14th Lok Sabha",
+      "15th Lok Sabha",
+      "16th Lok Sabha",
+      "17th Lok Sabha"
+    ]
+  },
+  {
+    numb: 6,
+    question: "The Parliament of India can make use of the residuary powers",
+    answer: "at all times",
+    options: [
+      "at all times",
+      "only during national emergency",
+      "during national emergency as well as constitutional emergency as well in a state",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 7,
+    question: "The members of Lok Sabha hold office for a term of",
+    answer: "5 years",
+    options: [
+      "4 years",
+      "5 years",
+      "6 years",
+      "3 years"
+    ]
+  },
+  {
+    numb: 8,
+    question: "The number of writs that can be prayed for and issued by the Supreme Court and/or a High Court is",
+    answer: "5",
+    options: [
+      "3",
+      "4",
+      "5",
+      "6"
+    ]
+  },
+  {
+    numb: 9,
+    question: "The minimum age to qualify for election to the Lok Sabha is",
+    answer: "25 years",
+    options: [
+      "25 years",
+      "21 years",
+      "18 years",
+      "35 years"
+    ]
+  },
+  {
+    numb: 10,
+    question: "The pension of a high court judge is charged to the",
+    answer: "Consolidated Fund of India",
+    options: [
+      "Consolidated Fund of India",
+      "Consolidated Fund of the state where he last served",
+      "Consolidated Funds of the different states where he has served",
+      "Contingency Fund of India"
+    ]
+  },
+];
+
